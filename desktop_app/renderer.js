014960cba519cfcc88170390ef217feae1bdf62d
Fahmi <fahmiomer02@gmail.com>
2025-10-07 01:27:39 -0400

    Update renderer.js

--- desktop_app/renderer.js
+++ desktop_app/renderer.js
@@ -131,7 +131,7 @@ async function init() {
         // Set fallback version
         const versionTag = document.getElementById('versionTag');
         if (versionTag) {
-          versionTag.textContent = 'v0.1.8';
+          versionTag.textContent = 'v0.1.9';
         }
       }
     }
